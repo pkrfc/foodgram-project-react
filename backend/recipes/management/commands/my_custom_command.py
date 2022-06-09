c1f34cfa5524aa91fa1d41c8bb499100f23e19c2
pkrfc <Happyz1804>
2022-06-09 13:23:30 +0300

    команда для заполнения бд

---
+++ backend/recipes/management/commands/my_custom_command.py
@@ -0,0 +1,23 @@
+import json
+
+from recipes.models import Ingredient
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open(
+                'data/ingredients.json', encoding='utf-8'
+        ) as json_file:
+            ingredients = json.load(json_file)
+            for ingredient in ingredients:
+                name = ingredient['name']
+                measurement_unit = ingredient['measurement_unit']
+                Ingredient.objects.create(
+                    name=name,
+                    measurement_unit=measurement_unit
+                )
+
+
+app = Command()
+app.handle()
