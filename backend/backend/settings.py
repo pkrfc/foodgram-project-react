5dc43b2b45853b3837d9757282f0ad5c75be6875
pkrfc <Happyz1804>
2022-06-27 10:56:46 +0300

    Подготовка инфрастуктуры

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,8 +114,6 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend"
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
