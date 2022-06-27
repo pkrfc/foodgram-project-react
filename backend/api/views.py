532da341d74dc45d25eb9dcd1a8aa05c6a8e92fc
pkrfc <Happyz1804>
2022-06-27 10:37:02 +0300

    Подготовка инфрастуктуры

--- backend/api/views.py
+++ backend/api/views.py
@@ -89,6 +89,7 @@ class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     permission_classes = (IsAuthorOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
+    """filter_class = RecipeFilter"""
     pagination_class = CustomPagination
 
     def get_serializer_class(self):
