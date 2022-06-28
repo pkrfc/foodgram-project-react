a25a6556137216e4ff40b942852cb30bc1ca8c7e
pkrfc <Happyz1804>
2022-06-28 12:24:23 +0300

    правки после деплоя

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -47,7 +47,7 @@ class SubscriptionsSerializer(CustomUserSerializer):
         return RecipeInfoSerializer(obj.recipes.all(), many=True).data
 
     def get_recipes_count(self, obj):
-        return Recipe.objects.filter(author=obj.user).count()
+        return Recipe.objects.filter(author=obj.following).count()
 
 
 class SubscribeSerializer(serializers.ModelSerializer):
